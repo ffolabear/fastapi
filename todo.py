1919ae38b0ee6d4a2474ea7e52bf641a9cf53a54
ffolabear <ffolabear@gmail.com>
2024-07-26 23:57:47 +0900

    feat : chapter2 예제 추가

--- todo.py
+++ todo.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Path
+
+from model import Todo
 
 todo_router = APIRouter()
 
@@ -6,7 +8,7 @@ todo_list = []
 
 
 @todo_router.post("/todo")
-async def add_todo(todo: dict) -> dict:
+async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {
         "message": "Todo added successfully"
@@ -17,4 +19,16 @@ async def add_todo(todo: dict) -> dict:
 async def retrieve_todos() -> dict:
     return {
         "todos": todo_list
+    }
+
+
+@todo_router.get("/todo/{todo_id}")
+async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to retrieve.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {
+                "todo": todo
+            }
+    return {
+        "message": "Todo with supplied ID doesn't exist"
     }
