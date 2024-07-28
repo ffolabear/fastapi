3985ed2e53ab09548db29cb190cdb9f95b8a2f68
ffolabear <ffolabear@gmail.com>
2024-07-28 23:20:55 +0900

    feat : chapter2 예제 추가

--- model.py
+++ model.py
@@ -7,8 +7,19 @@ class Todo(BaseModel):
 
     class Config:
         schema_extra = {
-            "example" : {
-                "id" : 1,
+            "example": {
+                "id": 1,
                 "item": "Example Schema!",
+            }
+        }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book",
             }
         }
