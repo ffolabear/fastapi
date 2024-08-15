c53d433a1d2589c0aa0ae31ef4687ccd9c96d626
ffolabear <ffolabear@gmail.com>
2024-08-15 23:35:15 +0900

    feat : OreillyFastAPI Chapter3 예제 추가

---
+++ OreillyFastAPI/hello.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/hi/{who}")
+def greet(who):
+    return f"Hello? {who}"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("hello:app", reload=True)
