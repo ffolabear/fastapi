70fe6268dec00ddb9900e9ab4410838584ea9b03
ffolabear <ffolabear@gmail.com>
2024-08-16 23:53:25 +0900

    feat : OreillyFastAPI Chapter3 예제 추가

--- OreillyFastAPI/hello.py
+++ OreillyFastAPI/hello.py
@@ -1,16 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Header
 
 app = FastAPI()
 
 
 @app.get("/hi/{who}")
-def greet(who):
+def greet_url(who):
     return f"Hello? {who}"
 
 
 @app.get("/hi")
-def greet(who):
+def greet_query_param(who):
     return f"Hello? {who}"
+
+
+@app.post("/hi")
+def greet_body(who: str = Body(embed=True)):
+    return f"Hello? {who}"
+
+
+@app.get("/header")
+def greet_header(who: str = Header()):
+    return f"Hello? {who}"
+
+
+@app.get("/agent")
+def get_agent(user_agent: str = Header()):
+    return user_agent
+
+
+@app.get("/happy")
+def happy(status_code=200):
+    return ":)"
+
 
 
 if __name__ == "__main__":
